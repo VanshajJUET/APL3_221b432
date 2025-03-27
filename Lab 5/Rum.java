31381f1e377c98d83bc5cb6f98b45a20e073e3f5
221B432 <221B432@juet.co.in>
2025-03-27 14:27:36 +0530

    LE 5

---
+++ Lab 5/Rum.java
@@ -0,0 +1,8 @@
+class Rum extends Beverage{
+    protected void addCondiment(){
+        System.out.println("Add Diet Coke");
+    }
+    protected void Stir(){
+        System.out.println("Stir it for 20 seconds");
+    }
+}
